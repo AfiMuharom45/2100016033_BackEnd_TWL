aab2c6ccc8f826f917014b76b361ed95f6e2338f
AlfiMuharom <Alfimuharom65@gmail.com>
2023-06-21 10:19:31 +0700

    update prak7

--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-// middleware/logger.js
-
-const logger = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  };
-  
-  module.exports = logger;
-  
